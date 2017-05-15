e92dd46ee6b7bae4431d1cab2b04aaf535d07786
xschen <xs0040@gmail.com>
2017-05-15 20:59:08 +0800

    More unit tests

--- src/main/java/com/github/chen0040/data/text/VocabularyTableCell.java
+++ src/main/java/com/github/chen0040/data/text/VocabularyTableCell.java
@@ -13,6 +13,7 @@ import java.io.Serializable;
 @Getter
 @Setter
 public class VocabularyTableCell implements Serializable {
+    private static final long serialVersionUID = 2293350262940297176L;
     private String word;
     private int wordIndex;
     private int count;
---
+++ src/test/java/com/github/chen0040/data/text/LowerCaseUnitTest.java
@@ -0,0 +1,39 @@
+package com.github.chen0040.data.text;
+
+
+import com.github.chen0040.data.text.utils.FileUtils;
+import org.testng.annotations.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.testng.Assert.*;
+
+
+/**
+ * Created by xschen on 15/5/2017.
+ */
+public class LowerCaseUnitTest {
+
+   @Test
+   public void test() throws IOException {
+      TextFilter filter = new LowerCase();
+
+
+
+      InputStream inputStream = FileUtils.getResource("documents.txt");
+      BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+      String content = reader.lines().collect(Collectors.joining("\n"));
+      reader.close();
+
+      List<String> before = BasicTokenizer.doTokenize(content);
+      List<String> after = filter.filter(before);
+
+      assertThat(before).isNotEqualTo(after);
+   }
+}
